2991b614d0c354006ac1c73610fc43f026895b2a
vitorbborges <vitorbborges31@gmail.com>
2025-06-25 21:46:05 +0200

    requirements

--- src/data_processing/feature_imputer.py
+++ src/data_processing/feature_imputer.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from fancyimpute import SoftImpute
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import OrdinalEncoder
 
 
 class FeatureImputer:
@@ -11,11 +14,6 @@ class FeatureImputer:
     - impute_gdf: soft-matrix completion for spatial attribute tables.
     - impute_edges: MICE-based imputation for graph edge attributes.
     """
-
-    def __init__(self) -> None:
-        from fancyimpute import SoftImpute
-        from sklearn.impute import IterativeImputer
-        from sklearn.preprocessing import OrdinalEncoder
 
     @staticmethod
     def impute_gdf(
